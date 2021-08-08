c0e4919aca9b1ead21ce99391f22136cdb77202e
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-08 14:01:59 +0800

    rolex: sepolicy: Label device nodes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,8 @@ TARGET_KERNEL_CONFIG := mi8937_defconfig
 # Security patch level
 VENDOR_SECURITY_PATCH := 2018-07-01
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 include vendor/xiaomi/rolex/BoardConfigVendor.mk
